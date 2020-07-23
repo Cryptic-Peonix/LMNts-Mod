227bc057ab4f600731f89f7aa0d966a506ab4cfd
Skyla <52376203+Cryptic-Peonix@users.noreply.github.com>
2020-07-22 22:08:56 -0400

    Add files via upload

--- src/main/java/com/triumvirate/lmnts/init/BlockInit.java
+++ src/main/java/com/triumvirate/lmnts/init/BlockInit.java
@@ -1,5 +1,17 @@
 package com.triumvirate.lmnts.init;
 
+import com.triumvirate.lmnts.LMNts;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class BlockInit {
 
+	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, LMNts.MOD_ID);
+
+	public static final RegistryObject<Block> TEST_BLOCK = BLOCKS.register("test_block",
+			() -> new Block(Block.Properties.from(Blocks.COBBLESTONE)));
 }
--- src/main/java/com/triumvirate/lmnts/init/ItemInit.java
+++ src/main/java/com/triumvirate/lmnts/init/ItemInit.java
@@ -1,5 +1,16 @@
 package com.triumvirate.lmnts.init;
 
+import com.triumvirate.lmnts.LMNts;
+import com.triumvirate.lmnts.LMNts.ModItemGroup;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ItemInit {
 
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, LMNts.MOD_ID);
+	
+	public static final RegistryObject<Item> TEST_ITEM = ITEMS.register("test_item", () -> new Item(new Item.Properties().group(ModItemGroup.lmnts).maxStackSize(64)));
 }

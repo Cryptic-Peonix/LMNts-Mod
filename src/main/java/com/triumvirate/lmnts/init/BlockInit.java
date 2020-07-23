5ef45304b627dfd7fbd13b921a14843f2642c366
Skyla <52376203+Cryptic-Peonix@users.noreply.github.com>
2020-07-22 22:46:48 -0400

    Add files via upload

--- src/main/java/com/triumvirate/lmnts/init/BlockInit.java
+++ src/main/java/com/triumvirate/lmnts/init/BlockInit.java
@@ -4,6 +4,9 @@ import com.triumvirate.lmnts.LMNts;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,4 +17,8 @@ public class BlockInit {
 
 	public static final RegistryObject<Block> TEST_BLOCK = BLOCKS.register("test_block",
 			() -> new Block(Block.Properties.from(Blocks.COBBLESTONE)));
+
+	public static final RegistryObject<Block> REINFORCED_STONE = BLOCKS.register("reinforced_stone",
+			() -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(10f, 30f).harvestLevel(2)
+					.harvestTool(ToolType.PICKAXE).func_235861_h_().sound(SoundType.STONE)));
 }

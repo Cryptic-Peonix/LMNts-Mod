50dd75532ce4c74147bdf82c3f0e8bfa124e61aa
Skyla <52376203+Cryptic-Peonix@users.noreply.github.com>
2020-07-22 22:09:33 -0400

    Updated Main Class

--- src/main/java/com/triumvirate/lmnts/LMNts.java
+++ src/main/java/com/triumvirate/lmnts/LMNts.java
@@ -1,41 +1,74 @@
 package com.triumvirate.lmnts;
 
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.triumvirate.lmnts.init.BlockInit;
+import com.triumvirate.lmnts.init.ItemInit;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
+import net.minecraftforge.registries.IForgeRegistry;
 
 
 @Mod("lmnts")
+@Mod.EventBusSubscriber(modid = LMNts.MOD_ID, bus = Bus.MOD)
 public class LMNts
 {
 
     private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "lmnts";
+    public static LMNts lmnts;
 
     public LMNts() {
-        // Register the setup method for modloading
+    	
+    	final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    	
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
+        BlockInit.BLOCKS.register(modEventBus);
+        ItemInit.ITEMS.register(modEventBus);
+        
+        lmnts = this;
+        
         MinecraftForge.EVENT_BUS.register(this);
+    }
+    
+    @SubscribeEvent
+    public static void onRegisterBlockItems(final RegistryEvent.Register<Item> event) {
+    	final IForgeRegistry<Item> registry = event.getRegistry();
+    	
+    	BlockInit.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(block -> {
+    		final Item.Properties properties = new Item.Properties().group(ModItemGroup.lmnts);
+    		final BlockItem blockItem = new BlockItem(block, properties);
+    		blockItem.setRegistryName(block.getRegistryName());
+    		registry.register(blockItem);;
+    	});
+    	LOGGER.debug("Registered Block Items");
     }
 
     private void setup(final FMLCommonSetupEvent event)
@@ -76,5 +109,19 @@ public class LMNts
         public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
             LOGGER.info("HELLO from Register Block");
         }
+    }
+    
+    //mod item group
+    public static class ModItemGroup extends ItemGroup {
+    	
+    	public static final ModItemGroup lmnts = new ModItemGroup(ItemGroup.GROUPS.length, "LMNts");
+    	private ModItemGroup(int index, String label) {
+    		super(index, label);
+    	}
+    	
+    	@Override
+    	public ItemStack createIcon() {
+    		return new ItemStack(Items.DIRT);
+    	}
     }
 }
